769e0bd9aedf669672471a972bda47e53c9493e9
Vita <theprototype@Vitas-MBP.lan>
2024-06-27 19:56:31 -0500

    finishedhomework

--- day-1.js
+++ day-1.js
@@ -12,11 +12,14 @@ function day1Part1() {
   // Try it!
   // TODO: add a new question for the player. Assign the answer to the playerOne Object.
 
+  playerOne.strength = prompt("What is your best strength?");
+  playerOne.goal = prompt("What is your ultimate goal?");
+
 
   // Try it!
   // TODO: add the answer to the new question here.
   messageParagraph.innerHTML = `Hi, ${playerOne.name}! Can you beat your hi score of ${playerOne.hiScore}?`;
-
+  messageParagraph.innerHTML = `We need more pros with ${playerOne.strength} like you. How do you plan to achieve your goal of ${playerOne.goal}?`;
 }
 
 part1.onclick = day1Part1;
@@ -29,11 +32,15 @@ function day1Part2() {
   // Try it!
   // TODO: add a new question for the player. Assign the answer to the playerOne Object.
   
+  customer.number = prompt("What is your phone number?");
+  customer.bday = prompt("What is your date of birth?");
 
   
   // Try it!
-  // TODO: add the answer to the new question to the innerHTML statment below.
+  // TODO: add the answer to the new question to the innerHTML statement below.
   messageParagraph.innerHTML = `Hi, ${customer.name}! Your current address is ${customer.address}.`;
+  messageParagraph.innerHTML = `${customer.name}, you updated your profile! Your number is now saved as ${customer.number}.`;
+  messageParagraph.innerHTML = `To confirm, ${customer.name}, we have updated your birthday to ${customer.bday}.`;
 }
 part2.onclick = day1Part2;
 
@@ -44,10 +51,13 @@ function day1Part3() {
 
   // Try it!
   // TODO: create a prompt to ask about a pet
+  pet.type = prompt("What type of pet do you have?");
+  pet.name = prompt("What is your pet's name?");
+  pet.food = prompt("What is your pet's fave food or treat?");
 
   // Try it!
   // TODO: make a message based on the answers to the prompt.
-  messageParagraph.innerHTML = ``;
+  messageParagraph.innerHTML = `I'll bet your ${pet.type}, ${pet.name} is just the most adorable thing ever! They deserve all the ${pet.food} their heart desires.`;
 }
 part3.onclick = day1Part3;
 
@@ -55,8 +65,14 @@ part3.onclick = day1Part3;
 // Try it!
 // TODO: add your own idea for a prompt. Use an object to store the user's response.
 function day1Part4() {
-  alert("hello!");
+  let movie;
+  movie = {};
 
+  movie.genre = prompt("What's your favorite movie genre?");
+  movie.alltime = prompt("What's your fave movie of ALL time?");
+  movie.classic = prompt("What movie is a classic and must-have for you?");
+
+  messageParagraph.innerHTML = `I like ${movie.genre} movies too, and I must admit ${movie.alltime} and ${movie.classic} are both great films.`
   
 }
 
--- day-2.js
+++ day-2.js
@@ -28,16 +28,15 @@ function day2Part2() {
   let bankNumber;
   let carNumber = 5000;
 
-  alert("Try it!");
-  // Try it!
+  
   // TODO: Prompt the user for how much in in their bank account. Convert it to a number.
   // 1. Ask the user for data
-
+  bank = prompt("What is the available balance of your bank account right now?");
   // 2. convert it to a number
-  
+  bankNumber = Number(bank);
 
   // 3. use a math expression with the > operator to say if the statements is true or false.
-  messageParagraph.innerHTML = `The car costs ${carNumber}. You have ${bankNumber} in your bank. You can afford the car: ${bankNumber > carNumber}`
+  messageParagraph.innerHTML = `The car costs $${carNumber}. You have ${bankNumber} in your bank. You can afford the car: ${bankNumber > carNumber}`
   
   
 }
@@ -49,18 +48,17 @@ function day2Part3() {
   let walletNumber;
   let tvNumber = 300;
 
-  alert("Try it!");
   // Try it!
   // TODO: Prompt the user for how much in in their wallet. Convert it to a number, and tell them true or false if they can buy a TV.
 
   // 1. Ask the user for data
-
+  wallet = prompt("How much do you have in your wallet?");
   // 2. convert it to a number
-
+  walletNumber = Number(wallet);
 
   // 3. use a math expression with the > operator to say if the statements is true or false.
 
-
+  messageParagraph.innerHTML = `The price of the TV you selected is $${tvNumber}; you have $${walletNumber} on hand. You can afford the TV: ${walletNumber > tvNumber}.`;
 }
 part3.onclick = day2Part3;
 
@@ -70,16 +68,15 @@ function day2Part4() {
   let bankNumber;
   let rocketNumber = 10000000.99;
 
-  alert("Try it!");
   // Try it!
   // TODO: Prompt the user for how much in in their bank account. Convert it to a number, and tell them true or false if they can buy a rocket.
   // 1. Ask the user for data
-
+  bank = prompt("What is your current balance in your bank account?");
   // 2. convert it to a number
-
+  bankNumber = Number(bank);
 
   // 3. use a math expression with the > operator to say if the statements is true or false.
 
-
+  messageParagraph.innerHTML = `The price of this particular rocket in your cart is $${rocketNumber}; you currently have $${bankNumber} available. Can you afford the rocket? Result: ${bankNumber > rocketNumber}`;
 }
 part4.onclick = day2Part4;
